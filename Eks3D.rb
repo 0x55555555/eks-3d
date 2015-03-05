77b0b736c544c9702e1e6be15856783b48cb3f69
George Pimm <jorjpimm@gmail.com>
2015-03-05 22:01:40 +0000

    fix return

--- Eks3D.rb
+++ Eks3D.rb
@@ -1,26 +1,26 @@
 RbMake.import_module('../EksCore')
 
-if (!RbMake.module_exists?(:QtCore))
-  return
-end
+if (RbMake.module_exists?(:QtCore))
 
-RbMake.library(:Eks3D, :Eks) do |l, p|
-  l.dependencies << :EksCore << :QtCore
-
-  l.export do |l|
+  RbMake.library(:Eks3D, :Eks) do |l, p|
     l.dependencies << :EksCore << :QtCore
+
+    l.export do |l|
+      l.dependencies << :EksCore << :QtCore
+
+      l.when(l.config.platform(:win)) do |l|
+        l.cpp.defines << "X_ENABLE_DX_RENDERER"
+      end
+      
+      l.cpp.defines << "X_ENABLE_GL_RENDERER"
+    end
 
     l.when(l.config.platform(:win)) do |l|
       l.cpp.defines << "X_ENABLE_DX_RENDERER"
     end
-    
+
     l.cpp.defines << "X_ENABLE_GL_RENDERER"
+    l.dependencies << :opengl
   end
 
-  l.when(l.config.platform(:win)) do |l|
-    l.cpp.defines << "X_ENABLE_DX_RENDERER"
-  end
-
-  l.cpp.defines << "X_ENABLE_GL_RENDERER"
-  l.dependencies << :opengl
-end 
+end
